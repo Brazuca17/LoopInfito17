46778eb01a414866d05c959b83f744ed375b6280
gustavo <tavinhocassis@gmail.com>
2024-11-12 11:38:39 -0300

    trabalho de ds

--- LOOPINFINIO17/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/LOOPINFINIO17.AssemblyInfo.cs
+++ LOOPINFINIO17/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/LOOPINFINIO17.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LOOPINFINIO17")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+dfddf6de738e73cfa9343c25db2e2659b37e7a18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+30293d268867ab0aaa4767dd168d5f323bfec36e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LOOPINFINIO17")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LOOPINFINIO17")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
